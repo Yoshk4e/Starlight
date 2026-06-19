aed6ea69d3ebece9603674b9abb0160068bc0bf5
Yoshk4e <105790589+Yoshk4e@users.noreply.github.com>
2026-06-19 20:14:50 +0300

    misc(SDK.Common): rename ApplicationId enum values

--- Source/Starlight.SDK/Common/ApplicationId.cs
+++ Source/Starlight.SDK/Common/ApplicationId.cs
@@ -3,11 +3,11 @@ namespace Starlight.SDK.Common;
 public enum ApplicationId
 {
     /// <summary>Production client.</summary>
-    GIRelease = 4,
+    Release = 4,
 
     /// <summary>Overseas sandbox / beta channel.</summary>
-    GISandboxOverseas = 9,
+    SandboxOS = 9,
 
     /// <summary>Chinese sandbox / beta channel.</summary>
-    GISandboxChina = 10
+    SandboxCN = 10
 }
